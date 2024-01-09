84a94f8e23a352a0f1fbf8a8441fb879b3e0afac
fajarhidayatt <fadjhar10@gmail.com>
2024-01-09 20:56:42 +0700

    create `Checkout` model and migrations

---
+++ app/Models/Checkout.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Checkout extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'camp_id',
+        'discount_id',
+        'discount_percentage',
+        'total',
+        'payment_status',
+        'midtrans_url',
+        'midtrans_booking_code',
+    ];
+}
---
+++ database/migrations/2024_01_09_133818_create_checkouts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('checkouts', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('camp_id')->constrained();
+            $table->foreignId('discount_id')->nullable();
+            $table->unsignedInteger('discount_percentage')->nullable();
+            $table->unsignedInteger('total')->default(0);
+            $table->string('payment_status', 100)->default('waiting');
+            $table->string('midtrans_url')->nullable();
+            $table->string('midtrans_booking_code')->nullable();
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('checkouts');
+    }
+};
