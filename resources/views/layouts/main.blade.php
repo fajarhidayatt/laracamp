100fa6da59e204adfd569b69720f432c1350155f
fajarhidayatt <fadjhar10@gmail.com>
2024-01-10 20:50:42 +0700

    style: slicing navbar and main layout

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laracamp</title>
+
+    {{-- Bootstrap --}}
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+        crossorigin="anonymous">
+
+    {{-- Custom CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}" type="text/css">
+
+    {{-- Poppins Google Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap">
+
+</head>
+
+<body>
+    @if (!Route::is('login') && !Route::is('checkout.success'))
+        @include('partials.navbar')
+    @endif
+
+    @yield('content')
+
+    {{-- Bootstrap --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,53 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+        <a class="navbar-brand" href="{{ route('home') }}">
+            <img src="{{ asset('images/logo.png') }}" alt="laracamp logo">
+        </a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Program</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Mentor</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Business</a>
+                </li>
+            </ul>
+            @php
+                $isLogin = true;
+            @endphp
+            @if ($isLogin)
+                <div class="d-flex user-logged">
+                    <a href="#">
+                        Halo, Beatrice!
+                        <img src="{{ asset('images/user_photo.png') }}" class="user-photo" alt="photo-profile">
+                    </a>
+                </div>
+            @else
+                <div class="d-flex">
+                    <a href="{{ route('login') }}" class="btn btn-master btn-secondary me-3">
+                        Sign In
+                    </a>
+                    <a href="#" class="btn btn-master btn-primary">
+                        Sign Up
+                    </a>
+                </div>
+            @endif
+        </div>
+    </div>
+</nav>
