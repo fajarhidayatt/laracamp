cc9fe183f8d53898109e1db74bc552113c8a9a91
fajarhidayatt <fadjhar10@gmail.com>
2024-01-10 21:06:29 +0700

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,21 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.home');
-})->name('home');
+    return view('welcome');
+});
 
-Route::get('/login', function () {
-    return view('pages.login');
-})->name('login');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/checkout', function () {
-    return view('pages.checkout');
-})->name('checkout');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/checkout/success', function () {
-    return view('pages.checkout-success');
-})->name('checkout.success');
-
-Route::get('/dashboard/user', function () {
-    return view('pages.user.dashboard');
-})->name('user');
+require __DIR__ . '/auth.php';
