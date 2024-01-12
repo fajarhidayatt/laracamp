b3724c41174fb68eb1edce75b3c1d9ac5b462454
fajarhidayatt <fadjhar10@gmail.com>
2024-01-12 22:37:06 +0700

    style: responsive login page

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,22 +1,26 @@
 @extends('layouts.main')
 
 @section('content')
-    <section class="login-user">
-        <div class="left">
-            <img src="{{ asset('images/ill_login_new.png') }}" alt="">
-        </div>
-        <div class="right">
-            <a href="{{ route('home') }}">
-                <img src="{{ asset('images/logo.png') }}" class="logo" alt="logo">
-            </a>
-            <h1 class="header-third">Start Today</h1>
-            <p class="subheader">Because tomorrow become never</p>
-            <p>
-                <a href="{{ route('login.google') }}" class="btn btn-border btn-google-login">
-                    <img src="{{ asset('images/ic_google.svg') }}" class="icon" alt="google"> Sign In with Google
+    <section class="login-user vw-100 vh-100 overflow-hidden">
+        <div class="row">
+            <div class="left vh-100 col-lg-6 d-none d-lg-flex align-items-center justify-content-center">
+                <img src="{{ asset('images/ill_login_new.png') }}" alt="">
+            </div>
+            <div
+                class="right vh-100 col-12 col-lg-6 d-flex flex-column align-items-center justify-content-center p-4 mx-auto">
+                <a href="{{ route('home') }}" class="align-self-start">
+                    <img src="{{ asset('images/logo.png') }}" class="logo" alt="logo">
                 </a>
-            </p>
-            <img src="{{ asset('images/people.png') }}" class="people" alt="">
+                <div class="align-self-start mt-4">
+                    <h1 class="header-third">Start Today</h1>
+                    <p class="subheader">Because tomorrow become never</p>
+                </div>
+                <a href="{{ route('login.google') }}" class="btn btn-border btn-google-login my-5">
+                    <img src="{{ asset('images/ic_google.svg') }}" class="icon" alt="google">
+                    Sign In with Google
+                </a>
+                <img src="{{ asset('images/people.png') }}" class="people" alt="">
+            </div>
         </div>
     </section>
 @endsection
