ee50ecef1fe1e0c86b5ef13b4744ed7dda2c71b6
fajarhidayatt <fadjhar10@gmail.com>
2024-01-12 22:37:28 +0700

    style: responsive checkout success page

--- resources/views/pages/checkout-success.blade.php
+++ resources/views/pages/checkout-success.blade.php
@@ -1,17 +1,13 @@
 @extends('layouts.main')
 
 @section('content')
-    <section class="checkout">
-        <div class="container">
-            <div class="row text-center">
-                <div class="col-lg-12 col-12">
-                    <img src="{{ asset('images/ill_register.png') }}" height="400" class="mb-5" alt=" ">
-                </div>
-                <div class=" col-lg-12 col-12 header-wrap mt-4">
-                    <p class="story">WHAT A DAY!</p>
-                    <h2 class="primary-header">Berhasil Checkout</h2>
-                    <a href="{{ route('user') }}" class="btn btn-primary mt-3">My Dashboard</a>
-                </div>
+    <section class="checkout-success container vh-100 d-flex align-items-center justify-content-center">
+        <div class="row text-center">
+            <img src="{{ asset('images/ill_register.png') }}" alt="checkout-success">
+            <div class="mt-5">
+                <p class="story">WHAT A DAY!</p>
+                <h2 class="primary-header">Berhasil Checkout</h2>
+                <a href="{{ route('user.dashboard') }}" class="btn btn-primary mt-3">My Dashboard</a>
             </div>
         </div>
     </section>
