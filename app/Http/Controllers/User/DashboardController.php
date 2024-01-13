c2ffb0f1836365f737d9331c6b6093b583d88d1b
fajarhidayatt <fadjhar10@gmail.com>
2024-01-13 20:45:23 +0700

    integration user dashboard

---
+++ app/Http/Controllers/User/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Checkout;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $userId = Auth::id();
+
+        $checkouts = Checkout::with('camp')
+            ->whereUserId($userId)
+            ->get();
+
+        return view('pages.user.dashboard', [
+            'checkouts' => $checkouts
+        ]);
+    }
+}
--- resources/views/pages/user/dashboard.blade.php
+++ resources/views/pages/user/dashboard.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <section class="dashboard my-5">
         <div class="container">
-            <div class="row text-left">
-                <div class=" col-lg-12 col-12 header-wrap mt-4">
+            <div class="row text-left mb-3">
+                <div class="header-wrap mt-4">
                     <p class="story">
                         DASHBOARD
                     </p>
@@ -13,83 +13,89 @@
                     </h2>
                 </div>
             </div>
-            <div class="row my-5">
+            @if (session()->has('error'))
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    {!! session('error') !!}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+
+            {{-- Desktop View --}}
+            <div class="row d-none d-lg-block">
                 <table class="table">
                     <tbody>
-                        <tr class="align-middle">
-                            <td width="18%">
-                                <img src="{{ asset('images/item_bootcamp.png') }}" height="120" alt="">
-                            </td>
-                            <td>
-                                <p class="mb-2">
-                                    <strong>Gila Belajar</strong>
-                                </p>
-                                <p>
-                                    September 24, 2021
-                                </p>
-                            </td>
-                            <td>
-                                <strong>$280,000</strong>
-                            </td>
-                            <td>
-                                <strong>Waiting for Payment</strong>
-                            </td>
-                            <td>
-                                <a href="#" class="btn btn-primary">
-                                    Get Invoice
-                                </a>
-                            </td>
-                        </tr>
-                        <tr class="align-middle">
-                            <td width="18%">
-                                <img src="{{ asset('images/item_bootcamp.png') }}" height="120" alt="">
-                            </td>
-                            <td>
-                                <p class="mb-2">
-                                    <strong>Gila Belajar</strong>
-                                </p>
-                                <p>
-                                    September 24, 2021
-                                </p>
-                            </td>
-                            <td>
-                                <strong>$280,000</strong>
-                            </td>
-                            <td>
-                                <strong><span class="text-green">Payment Success</span></strong>
-                            </td>
-                            <td>
-                                <a href="#" class="btn btn-primary">
-                                    Get Invoice
-                                </a>
-                            </td>
-                        </tr>
-                        <tr class="align-middle">
-                            <td width="18%">
-                                <img src="{{ asset('images/item_bootcamp.png') }}" height="120" alt=" ">
-                            </td>
-                            <td>
-                                <p class=" mb-2 ">
-                                    <strong>Gila Belajar</strong>
-                                </p>
-                                <p>
-                                    September 24, 2021
-                                </p>
-                            </td>
-                            <td>
-                                <strong>$280,000</strong>
-                            </td>
-                            <td>
-                                <strong><span class="text-red ">Canceled</span></strong>
-                            </td>
-                            <td>
-                                <a href="# " class="btn btn-primary ">
-                                    Get Invoice
-                                </a>
-                            </td>
-                        </tr>
+                        @forelse ($checkouts as $checkout)
+                            <tr class="align-middle">
+                                <td width="18%">
+                                    <img src="{{ asset('images/item_bootcamp.png') }}" height="120" alt="camp thumbnail">
+                                </td>
+                                <td>
+                                    <p class="mb-2">
+                                        <strong>{{ $checkout->camp->title }}</strong>
+                                    </p>
+                                    <p>
+                                        {{ $checkout->created_at->format('d M Y') }}
+                                    </p>
+                                </td>
+                                <td>
+                                    <strong>
+                                        Rp. {{ $checkout->total }}
+                                        @if ($checkout->discount_id)
+                                            <span class="badge bg-success">Disc {{ $checkout->discount_percentage }}%</span>
+                                        @endif
+                                    </strong>
+                                </td>
+                                <td>
+                                    <strong>{{ $checkout->payment_status }}</strong>
+                                </td>
+                                <td>
+                                    @if ($checkout->payment_status == 'waiting')
+                                        <a href="{{ $checkout->midtrans_url }}" class="btn btn-primary">Pay Here</a>
+                                    @else
+                                        <a href="#" class="btn btn-primary">
+                                            Get Invoice
+                                        </a>
+                                    @endif
+                                </td>
+                            </tr>
+                        @empty
+                            <tr class="text-center">
+                                <td colspan="5">No Camp Register</td>
+                            </tr>
+                        @endforelse
                     </tbody>
                 </table>
+            </div>
+
+            {{-- Mobile View --}}
+            <div class="row d-block d-lg-none">
+                @forelse ($checkouts as $checkout)
+                    <div class="mb-4 d-flex">
+                        <img src="{{ asset('images/item_bootcamp.png') }}" height="120" alt="camp thumbnail">
+                        <div class="d-flex flex-column px-3 py-2">
+                            <strong>{{ $checkout->camp->title }}</strong>
+                            <strong>
+                                Rp. {{ $checkout->total }}
+                                @if ($checkout->discount_id)
+                                    <span class="badge bg-success">Disc {{ $checkout->discount_percentage }}%</span>
+                                @endif
+                            </strong>
+                            <div class="mt-2">
+                                @if ($checkout->payment_status == 'waiting')
+                                    <a href="{{ $checkout->midtrans_url }}" class="btn btn-primary btn-sm">Pay Here</a>
+                                @else
+                                    <a href="#" class="btn btn-sm btn-primary">
+                                        Get Invoice
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                @empty
+                    <p class="text-center mt-5">
+                        No Camp Register
+                    </p>
+                @endforelse
             </div>
         </div>
     </section>
